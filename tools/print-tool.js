de4997b053f07d86b3a8cf0ed342d52a64fb2963
jpkt990410 <114401455+jpkt990410@users.noreply.github.com>
2022-11-05 17:03:32 +0900

    Add border for debugging

--- tools/print-tool.js
+++ tools/print-tool.js
@@ -3,35 +3,34 @@ const inputFile = document.querySelector('#input-file');
 const selectElement = document.querySelector('#select-paper-direction');
 const sectionImgs = document.querySelector('#section-imgs');
 const tcContainer = document.querySelector('#template-container').content;
-const tcImgContainer = document.querySelector('#template-img-container').content;
+const tcImgBox = document.querySelector('#template-img-box').content;
 
-let classesMain = document.querySelector('main').classList;
 const PAGE_DIRECTIONS = ['portrait', 'landscape'];
 
 const updatePage = () => {
-  //Set the page size.
-  styleElement.textContent =
-    '@page { size: A4 ' + PAGE_DIRECTIONS[selectElement.selectedIndex] + '; }';
-  //Fallback for environments which doesn't support size descriptor.
-  classesMain.remove('a4-portrait');
-  classesMain.remove('a4-landscape');
-  classesMain.add('a4-' + PAGE_DIRECTIONS[selectElement.selectedIndex]);
-
   //Create a new container.
   let newContainer = tcContainer.cloneNode(true);
   for (let file of inputFile.files) {
-    let newImgContainer = tcImgContainer.cloneNode(true);
-    newImgContainer.querySelector('img').src = URL.createObjectURL(file);
-    newContainer.querySelector('div').appendChild(newImgContainer);
+    let newImgBox = tcImgBox.cloneNode(true);
+    newImgBox.querySelector('img').src = URL.createObjectURL(file);
+    newContainer.querySelector('div').appendChild(newImgBox);
   }
 
   //Replace the old container.
   sectionImgs.querySelector('div').replaceWith(newContainer);
+
+  //Set the page size.
+  styleElement.textContent =
+    '@page { size: A4 ' + PAGE_DIRECTIONS[selectElement.selectedIndex] + '; }';
+  //Fallback for environments which doesn't support size descriptor.
+  sectionImgs.querySelector('div').classList.add(
+    'a4-' + PAGE_DIRECTIONS[selectElement.selectedIndex]
+  );
 }
 
 //Call updatePage() for initialization.
-updatePage();
+//updatePage();
 
 //Add event listeners
-selectElement.addEventListener('change', updatePage);
+//selectElement.addEventListener('change', updatePage);
 inputFile.addEventListener('change', updatePage);
